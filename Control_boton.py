5165ecc49f0c5f8b165eac66a671c41391596a3a
Cosmo125 <102827802+Cosmo125@users.noreply.github.com>
2024-01-23 19:44:43 -0500

    Create Control_boton.py

---
+++ Control_boton.py
@@ -0,0 +1,27 @@
+#Insertamso las librerias de trabajo
+from machine import Pin
+#libreria para el tiempo
+import time
+
+#Asignacion de bariables
+red = 2
+bot = 13
+
+#Asignacion de salidas y entradas 
+led = Pin(red, Pin.OUT)
+boton = Pin(bot, Pin.IN)
+
+#inicio e sistema
+print("GO")
+
+#inicia un bucle para que se repita
+while True:
+
+    #Inicio de las sentencias de pregunta
+    if boton.value() == 1:
+        led.value(1)
+    else:
+        led.value(0)
+        
+    #esperamos un tiempo para evitar rebotes
+    time.sleep(0.1)
