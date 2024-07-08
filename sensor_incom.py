d8b3b54b5145d4ac16806ba27ed271f9ab757de5
Cosmo125 <102827802+Cosmo125@users.noreply.github.com>
2024-07-08 11:35:26 -0500

    Update sensor_incom.py

--- sensor_incom.py
+++ sensor_incom.py
@@ -1,5 +1,5 @@
 #importo libreras
-from machine import PWM, Pin
+from machine import PWM, Pin, Timer
 import time
 
 #configuro pines
@@ -7,7 +7,7 @@ cont1 = Pin(19,Pin.OUT)#control direcion
 cont2 = Pin(21,Pin.OUT)#control direccion
 pwm = PWM(Pin(18))#velocidad
 pwm.freq(1000)#frecuencia
-sensor = Pin(5, Pin.IN) #sensor
+sensor = Pin(13, Pin.IN) #sensor
 #inicio motor
 
 cont1.value(0)
@@ -16,6 +16,27 @@ cont2.value(0)
 #programa principal
 contador = 0
 vueltas = 0
+rps = 0
+timer = Timer(0)
+
+
+def contar(pin):
+    global contador
+    global vueltas
+    contador += 1
+    
+    if contador == 20:
+        vueltas += 1
+        contador = 0
+    
+def rps(timer):
+    global vueltas, rps
+    rps = vueltas
+    vueltas = 0
+    print(rps)
+    
+sensor.irq(handler = contar, trigger = Pin.IRQ_FALLING)
+timer.init(period=1000, mode=Timer.PERIODIC, callback=rps)
 
 while True:
     #configuramos velocidad minima 0 maxima 1023
@@ -23,14 +44,11 @@ while True:
     #configuramos direcion de velocidad
     cont1.value(0)
     cont2.value(1)
-    if sensor.value() == 1:
-        contador += 1
-    if contador == 20:
-        vueltas += 1
-        contador = 0
+
+    
 
 
-    print(tiempo_transcurrido)
+
     
     
     
